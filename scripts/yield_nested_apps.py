7793fa0f2e65b12e1f78bd25760f2f5f25a205b0
Christoph Rieß <riess@carlfinance.de>
2022-05-04 15:38:50 +0200

    [fix] fix app name yielding from simple python script

--- scripts/yield_nested_apps.py
+++ scripts/yield_nested_apps.py
@@ -6,6 +6,6 @@ import os
 app_structure = json.load(open(f'{os.environ["BUILD_DIR"]}/app_structure.json'))
 
 if 'nested' in app_structure:
-    print(' '.join(list(map(lambda x: x[0], app_structure['nested']))))
+    print(' '.join(list(map(lambda x: x, app_structure['nested']))))
 else:
     print('nested_not_found')

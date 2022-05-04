45343e002d1c74195ea8445a2a2b082e7ee5a2e5
Christoph Rieß <riess@carlfinance.de>
2022-05-04 15:22:16 +0200

    [fix] in python access env through environ correctly

--- scripts/yield_installer_script.py
+++ scripts/yield_installer_script.py
@@ -3,7 +3,7 @@
 import json
 import os
 
-app_structure = json.load(open(f'{os.env["BUILD_DIR"]}/app_structure.json'))
+app_structure = json.load(open(f'{os.environ["BUILD_DIR"]}/app_structure.json'))
 
 if 'integration' in app_structure and app_structure['integration']:
     print('installer_integration.sh')
